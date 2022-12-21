379f3462b86aae1bd559cbc3c411683c7c77cb7d
Hugo Branco <hugobrancowb@gmail.com>
2022-12-20 23:45:07 -0300

    :tada: Creates project
    Signed-off-by: Hugo Branco <hugobrancowb@gmail.com>

--- pokedex/ContentView.swift
+++ pokedex/ContentView.swift
@@ -8,19 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+  @State var searchQuery = ""
+
+  var body: some View {
+    NavigationView {
+      ScrollView {
+//        Image(systemName: "globe")
+//          .imageScale(.large)
+//          .foregroundColor(.accentColor)
+//
+//        Text("Hello, world!")
+      }
+      .toolbar {
+        // MARK: Title
+        ToolbarItem(placement: .navigationBarLeading) {
+          HStack(spacing: 16) {
+          Image("Pokeball")
+            .resizable()
+            .aspectRatio(1, contentMode: .fit)
+            .frame(width: 24)
+
+          Text("Pokédex")
+            .font(.system(size: 24, weight: .bold))
+          }
         }
-        .padding()
+
+        // MARK: Filter action
+        ToolbarItem(placement: .primaryAction) {
+          Image(systemName: "line.3.horizontal.decrease.circle")
+            .onTapGesture {
+              // TODO: Open filter Menu
+            }
+        }
+      }
     }
+    .searchable(text: $searchQuery, prompt: "Search")
+
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
