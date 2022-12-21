915dca06598cd288d4e499e08ad08cdcf6ae5595
Hugo Branco <hugobrancowb@gmail.com>
2022-12-20 22:01:27 -0300

    Initial Commit

---
+++ pokedex/pokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokedexApp.swift
+//  pokedex
+//
+//  Created by Hugo Branco on 20/12/22.
+//
+
+import SwiftUI
+
+@main
+struct pokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
