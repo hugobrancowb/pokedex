4166f0764a4f369fe4b8ecbf637d4fd691f4912f
Hugo Branco <hugobrancowb@gmail.com>
2022-12-21 19:26:12 -0300

    feat: Creates a mock for pokecard
    Signed-off-by: Hugo Branco <hugobrancowb@gmail.com>

---
+++ pokedex/Extension/Fonts.swift
@@ -0,0 +1,13 @@
+//
+//  Fonts.swift
+//  pokedex
+//
+//  Created by Hugo Branco on 21/12/22.
+//
+
+import SwiftUI
+
+extension Font {
+  static func pokebold(size: CGFloat) -> Font { Font.custom("Poppins-Bold", size: size) }
+  static func pokereg(size: CGFloat) -> Font { Font.custom("Poppins-Regular", size: size) }
+}
